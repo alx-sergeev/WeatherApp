f6d1ef3995a662af55e4feb28c10a9d125bcf4b1
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-24 12:10:42 +0400

    Initial Commit

---
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Сергеев Александр on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
