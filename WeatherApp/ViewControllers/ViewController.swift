7918421f8e5bb1ec1154ae38ba6072855fe0a1c4
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-24 19:09:09 +0400

    Определение местоположения пользователя, alert с вопросом

--- WeatherApp/ViewControllers/ViewController.swift
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -6,11 +6,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    private var locationManager = CLLocationManager()
+    private var currentLoc: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        locationManager.requestWhenInUseAuthorization()
+        if(locationManager.authorizationStatus == .authorizedWhenInUse || locationManager.authorizationStatus == .authorizedAlways) {
+            guard let currentLoc = locationManager.location else { return }
+            
+            let geoCoder = CLGeocoder()
+            geoCoder.reverseGeocodeLocation(currentLoc) { [weak self] placemarks, error in
+                guard let self = self, let placeMark = placemarks?.first else { return }
 
+                if let city = placeMark.subAdministrativeArea {
+                    self.confirmSelectCity(city)
+                }
+            }
+        }
+    }
+}
+
+extension ViewController {
+    func confirmSelectCity(_ city: String) {
+        let alert = UIAlertController(title: "Выбор города", message: "Ваш город \(city)?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Да", style: .default, handler: nil)
+        let otherAction = UIAlertAction(title: "Нет", style: .destructive, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(otherAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
