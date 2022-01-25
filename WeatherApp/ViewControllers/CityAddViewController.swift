2ed64c46417e3244361a04c28f538da0de53c8fa
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-25 17:52:48 +0400

    Динамический поиск города при добавлении (заготовленный массив городов)

--- WeatherApp/ViewControllers/CityAddViewController.swift
+++ WeatherApp/ViewControllers/CityAddViewController.swift
@@ -8,14 +8,67 @@
 import UIKit
 
 class CityAddViewController: UIViewController {
-    // MARK: IBOutlets
+    // MARK: - IBOutlets
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tasksTableView: UITableView!
+    
+    // MARK: - Properties
+    private let citiesData = ["Ульяновск", "Москва", "Казань"]
+    private var filteredData: [String]!
+    private let citySearchCell = "citySearchCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        searchBar.delegate = self
+        tasksTableView.dataSource = self
+        
+        filteredData = citiesData
     }
 
     @IBAction func cancelButtonPressed(_ sender: Any) {
         dismiss(animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CityAddViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: citySearchCell, for: indexPath)
+        var cellConfigurator = cell.defaultContentConfiguration()
+        
+        cellConfigurator.text = filteredData[indexPath.row]
+        
+        cell.contentConfiguration = cellConfigurator
+        
+        return cell
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension CityAddViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = []
+        
+        if searchText.isEmpty {
+            filteredData = citiesData
+        } else {
+            for city in citiesData {
+                if city.lowercased().contains(searchText.lowercased()) {
+                    filteredData?.append(city)
+                }
+            }
+        }
+        
+        tasksTableView.reloadData()
     }
 }
