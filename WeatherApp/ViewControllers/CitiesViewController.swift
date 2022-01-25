d8579443687b05a1669f8ddd143f97e75220dfa1
Alexander Sergeev <sergeev_ul73@mail.ru>
2022-01-25 19:25:33 +0400

    Вывод списка добавленных городов

--- WeatherApp/ViewControllers/CitiesViewController.swift
+++ WeatherApp/ViewControllers/CitiesViewController.swift
@@ -8,9 +8,40 @@
 import UIKit
 
 class CitiesViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var citiesTableView: UITableView!
+    
+    // MARK: - Properties
+    private let cityCell = "cityCell"
+    private let storageManager = StorageManager.shared
+    private lazy var getCities: [City] = storageManager.getCities()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        citiesTableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CitiesViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return getCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cityCell, for: indexPath)
+        var cellConfigurator = cell.defaultContentConfiguration()
+        
+        let city = getCities[indexPath.row]
+        cellConfigurator.text = city.name
+        
+        cell.contentConfiguration = cellConfigurator
+        
+        return cell
+    }
 }
